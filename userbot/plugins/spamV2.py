245223a9530794271545d2d020f3b1cbabb878e5
ajaypathakakp <49590931+ajaypathakakp@users.noreply.github.com>
2020-03-25 20:40:11 +0530

    Update spamV2.py

--- userbot/plugins/spamV2.py
+++ userbot/plugins/spamV2.py
@@ -6,7 +6,7 @@ from userbot.utils import admin_cmd
 @borg.on(admin_cmd("tspam"))
 async def tmeme(e):
      tspam = str(e.text[7:])
-     message = tspam.replace(" ", "")
+     message = tspam.replace("fuck", "madarchod", "randi", "lode", "laude")
      for letter in message:
          await e.respond(letter)
      await e.delete()
